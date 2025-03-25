9a2c43dbd40578f51a61529e9a5a25fcc3fef45c
juanchezz <sanchez.138940@e.unavarra.es>
2025-03-25 12:16:06 +0100

    clase2103

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,16 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+    public function add(string $numbers): int
+    {
+        if (str_constains($numbers,',')){
+            return array_sum(explode(',',$numbers));
+        }
+
+        if (empty($numbers)){
+            return 0;
+        }
+
+        return $numbers;
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -9,5 +9,31 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    private StringCalculator $stringCalculator;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+    /**
+     * @test
+     */
+    public function empty_string_returns_zero(): void
+    {
+        $this->assertEquals(0, $this->StringCalculator->add(""));
+    }
+    /**
+     * @test
+     */
+    public function givenOneNumberReturnsThisNumber(): void
+    {
+        $this->assertEquals(1, $this->StringCalculator->add("1"));
+    }
+    /**
+     * @test
+     */
+    public function givenTwoNumbersReturnsSum()
+    {
+        $this->assertEquals(3, $this->StringCalculator->add("1,2"));
+    }
 }
